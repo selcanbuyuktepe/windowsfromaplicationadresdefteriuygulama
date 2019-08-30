c01d14c6ba1cf2a02226a4e70c22472b400488c1
selcanbuyuktepe <54715927+selcanbuyuktepe@users.noreply.github.com>
2019-08-30 20:48:24 +0300

    adres defteri

---
+++ Adres Defteri/Adres Defteri/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Adres_Defteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
